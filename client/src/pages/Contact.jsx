f092d03a978c39484fbfae4a96f886d600c48d50
Tushar Vaishnav <135534368+Tusharvaishnav005@users.noreply.github.com>
2025-03-10 02:13:44 +0530

    Update Contact.jsx

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,92 +1,137 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useAuth } from "../store/auth";
-import { Link } from "react-router-dom";
 
-export const AdminUsers = () => {
-  const [users, setUsers] = useState([]);
+const defaultContactFormData = {
+  username: "",
+  email: "",
+  message: "",
+};
 
-  const { authorizationToken, API} = useAuth();
+export const Contact = () => {
+  const [contact, setContact] = useState(defaultContactFormData);
 
-  const getAllUsersData = async () => {
-    try {
-      const response = await fetch(`${API}/api/admin/users`, {
-        method: "GET",
-        headers: {
-          Authorization: authorizationToken,
-        },
-      });
-      const data = await response.json();
-      console.log(`users ${data}`);
-      setUsers(data);
-    } catch (error) {
-      console.log(error);
-    }
+  const [userData, setUserData] = useState(true);
+
+  const { user, API } = useAuth();
+
+  if (userData && user) {
+    setContact({
+      username: user.username,
+      email: user.email,
+      message: "",
+    });
+
+    setUserData(false);
+  }
+
+  // lets tackle our handleInput
+  const handleInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setContact({
+      ...contact,
+      [name]: value,
+    });
   };
 
-  //   delelte the user on delete button
-  const deleteUser = async (id) => {
+  // handle fomr getFormSubmissionInfo
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
     try {
-      const response = await fetch(`${API}/api/admin/users/delete/${id}`, {
-        method: "DELETE",
+      const response = await fetch(`${API}/api/form/contact`, {
+        method: "POST",
         headers: {
-          Authorization: authorizationToken,
+          "Content-Type": "application/json",
         },
+        body: JSON.stringify(contact),
       });
-      const data = await response.json();
-      console.log(`users after delete:  ${data}`);
 
       if (response.ok) {
-        getAllUsersData();
+        setContact(defaultContactFormData);
+        const data = await response.json();
+        console.log(data);
+        alert("Message send successfully");
       }
     } catch (error) {
+      alert("Message not send");
       console.log(error);
     }
   };
 
-  useEffect(() => {
-    getAllUsersData();
-  }, []);
   return (
     <>
-      <section className="admin-users-section">
-        <div className="container">
-          <h1>Admin Users Data </h1>
+      <section className="section-contact">
+        <div className="contact-content container">
+          <h1 className="main-heading">contact us</h1>
         </div>
-        <div className="container  admin-users">
-          <table>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Phone</th>
-                <th>Update</th>
-                <th>Delete</th>
-              </tr>
-            </thead>
-            <tbody>
-              {users.map((curUser, index) => {
-                return (
-                  <tr key={index}>
-                    <td>{curUser.username}</td>
-                    <td>{curUser.email}</td>
-                    <td>{curUser.phone}</td>
-                    <td>
-                      <Link to={`/admin/users/${curUser._id}/edit`}>Edit</Link>
-                    </td>
-                    <td>
-                      <button
-                        className="btn"
-                        onClick={() => deleteUser(curUser._id)}
-                      >
-                        Delete
-                      </button>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
+        {/* contact page main  */}
+        <div className="container grid grid-two-cols">
+          <div className="contact-img">
+            <img src="/images/support.png" alt="we are always ready to help" />
+          </div>
+
+          {/* contact form content actual  */}
+          <section className="section-form">
+            <form onSubmit={handleSubmit}>
+              <div>
+                <label htmlFor="username">username</label>
+                <input
+                  type="text"
+                  name="username"
+                  id="username"
+                  autoComplete="off"
+                  value={contact.username}
+                  onChange={handleInput}
+                  required
+                />
+              </div>
+
+              <div>
+                <label htmlFor="email">email</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  autoComplete="off"
+                  value={contact.email}
+                  onChange={handleInput}
+                  required
+                />
+              </div>
+
+              <div>
+                <label htmlFor="message">message</label>
+                <textarea
+                  name="message"
+                  id="message"
+                  autoComplete="off"
+                  value={contact.message}
+                  onChange={handleInput}
+                  required
+                  cols="30"
+                  rows="6"
+                ></textarea>
+              </div>
+
+              <div>
+                <button type="submit">submit</button>
+              </div>
+            </form>
+          </section>
         </div>
+
+        <section className="mb-3">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3782.2613173278896!2d73.91411937501422!3d18.562253982539413!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2c147b8b3a3bf%3A0x6f7fdcc8e4d6c77e!2sPhoenix%20Marketcity%20Pune!5e0!3m2!1sen!2sin!4v1697604225432!5m2!1sen!2sin"
+            width="100%"
+            height="450"
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </section>
       </section>
     </>
   );
