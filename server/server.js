443218780e722e4d127dce0b2f784828e473bea6
Tushar Vaishnav <135534368+Tusharvaishnav005@users.noreply.github.com>
2025-03-10 00:38:51 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ app.use("/api/admin", adminRoute);
 //   res.status(200).json({ msg: "registration successful" });
 // });
 app.use(errorMiddleware);
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 connectDb().then(()=>{
   app.listen(PORT, () => {
     console.log(`server is running at port: ${PORT}`);
